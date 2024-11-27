86a63b72cc1ac6a9b62cd441f0dec28d7a557364
mixelpixx <chris_stockslager@comcast.net>
2024-11-26 23:55:46 -0500

    Delete mcp_server_manager directory

--- mcp_server_manager/utils/constants.py
+++
@@ -1,5 +0,0 @@
-import os
-
-APP_DATA_FOLDER = os.path.join(os.environ['APPDATA'], 'Claude')
-MCP_SERVERS_FOLDER = os.path.join(APP_DATA_FOLDER, 'MCPServers')
-CONFIG_FILE_PATH = os.path.join(APP_DATA_FOLDER, 'CONFIG.JSON')
